4d91d12fea2083bad2b09530f6e0a562ff4aa141
Elias Barreiro Suris <ebarreiro@atsistemas.com>
2024-02-21 09:43:27 +0100

    config: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules))],
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideAnimationsAsync()],
 };
