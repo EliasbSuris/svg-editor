b93553a757e1744879e5dd6fe1bda28b47366cfe
Elias Barreiro Suris <ebarreiro@atsistemas.com>
2024-02-21 10:29:15 +0100

    feat: create basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconButton } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ROUTES_PATH } from './app.routes';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'aor-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatSidenavModule, MatIconButton, MatIcon],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'svg-editor';
+  public APP_PATHS = ROUTES_PATH;
+  public title = 'svg-editor';
 }
